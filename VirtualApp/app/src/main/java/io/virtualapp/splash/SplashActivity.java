b363bbd5625546ee485e8fa379f8535faa90e423
mostwanted <ash.luna.shei@gmail.com>
2026-09-24 00:28:56 +0330

    Remove legacy UI analytics dependencies

--- VirtualApp/app/src/main/java/io/virtualapp/splash/SplashActivity.java
+++ VirtualApp/app/src/main/java/io/virtualapp/splash/SplashActivity.java
@@ -6,29 +6,20 @@ import android.view.WindowManager;
 import com.lody.virtual.client.core.VirtualCore;
 
 import io.virtualapp.R;
-import io.virtualapp.VCommends;
 import io.virtualapp.abs.ui.VActivity;
 import io.virtualapp.abs.ui.VUiKit;
-import io.virtualapp.home.FlurryROMCollector;
 import io.virtualapp.home.HomeActivity;
-import jonathanfinerty.once.Once;
 
 public class SplashActivity extends VActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        @SuppressWarnings("unused")
-        boolean enterGuide = !Once.beenDone(Once.THIS_APP_INSTALL, VCommends.TAG_NEW_VERSION);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         VUiKit.defer().when(() -> {
-            if (!Once.beenDone("collect_flurry")) {
-                FlurryROMCollector.startCollect();
-                Once.markDone("collect_flurry");
-            }
             long time = System.currentTimeMillis();
             doActionInThread();
             time = System.currentTimeMillis() - time;
