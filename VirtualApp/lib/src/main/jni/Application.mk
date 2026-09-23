6dc073755f3520da71de6f9f0c30c6aac4d01452
mostwanted <ash.luna.shei@gmail.com>
2026-09-24 00:30:51 +0330

    Enable ARM64 with modern NDK C++ runtime

--- VirtualApp/lib/src/main/jni/Application.mk
+++ VirtualApp/lib/src/main/jni/Application.mk
@@ -1,6 +1,6 @@
-APP_ABI := armeabi-v7a x86
+APP_ABI := arm64-v8a armeabi-v7a x86
 APP_PLATFORM := android-14
-APP_STL := gnustl_static
+APP_STL := c++_static
 APP_OPTIM := release
-VA_ROOT          := $(call my-dir)
-NDK_MODULE_PATH  := $(NDK_MODULE_PATH):$(VA_ROOT)
+VA_ROOT := $(call my-dir)
+NDK_MODULE_PATH := $(NDK_MODULE_PATH):$(VA_ROOT)
