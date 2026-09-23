b5e74db682509c7a581deecd0c511e410f9d65dc
mostwanted <ash.luna.shei@gmail.com>
2026-09-24 01:13:29 +0330

    Restore ARM64 HookZz context trampolines

--- VirtualApp/lib/src/main/jni/HookZz/src/platforms/backend-arm64/thunker-arm64.c
+++ VirtualApp/lib/src/main/jni/HookZz/src/platforms/backend-arm64/thunker-arm64.c
@@ -32,76 +32,57 @@
 
 // 前提: 不能直接访问 pc, 也就说只有通过寄存器才能实现绝对地址跳
 
-// __attribute__((__naked__)) static void ctx_save() {
-//     __asm__ volatile(
+__attribute__((naked)) static void ctx_save() {
+    __asm__ volatile(
+        "sub sp, sp, #(8*16)\n"
+        "stp q6, q7, [sp, #(6*16)]\n"
+        "stp q4, q5, [sp, #(4*16)]\n"
+        "stp q2, q3, [sp, #(2*16)]\n"
+        "stp q0, q1, [sp, #(0*16)]\n"
+        "sub sp, sp, #(30*8)\n"
+        "stp x29, x30, [sp, #(28*8)]\n"
+        "stp x27, x28, [sp, #(26*8)]\n"
+        "stp x25, x26, [sp, #(24*8)]\n"
+        "stp x23, x24, [sp, #(22*8)]\n"
+        "stp x21, x22, [sp, #(20*8)]\n"
+        "stp x19, x20, [sp, #(18*8)]\n"
+        "stp x17, x18, [sp, #(16*8)]\n"
+        "stp x15, x16, [sp, #(14*8)]\n"
+        "stp x13, x14, [sp, #(12*8)]\n"
+        "stp x11, x12, [sp, #(10*8)]\n"
+        "stp x9, x10, [sp, #(8*8)]\n"
+        "stp x7, x8, [sp, #(6*8)]\n"
+        "stp x5, x6, [sp, #(4*8)]\n"
+        "stp x3, x4, [sp, #(2*8)]\n"
+        "stp x1, x2, [sp, #(0*8)]\n"
+        "sub sp, sp, #(2*8)\n"
+        "str x0, [sp, #8]\n");
+}
+__attribute__((naked)) static void ctx_restore() {
+    __asm__ volatile(
+        "ldr x0, [sp, #8]\n"
+        "add sp, sp, #(2*8)\n"
+        "ldp x1, x2, [sp], #16\n"
+        "ldp x3, x4, [sp], #16\n"
+        "ldp x5, x6, [sp], #16\n"
+        "ldp x7, x8, [sp], #16\n"
+        "ldp x9, x10, [sp], #16\n"
+        "ldp x11, x12, [sp], #16\n"
+        "ldp x13, x14, [sp], #16\n"
+        "ldp x15, x16, [sp], #16\n"
+        "ldp x17, x18, [sp], #16\n"
+        "ldp x19, x20, [sp], #16\n"
+        "ldp x21, x22, [sp], #16\n"
+        "ldp x23, x24, [sp], #16\n"
+        "ldp x25, x26, [sp], #16\n"
+        "ldp x27, x28, [sp], #16\n"
+        "ldp x29, x30, [sp], #16\n"
+        "ldp q0, q1, [sp], #32\n"
+        "ldp q2, q3, [sp], #32\n"
+        "ldp q4, q5, [sp], #32\n"
+        "ldp q6, q7, [sp], #32\n");
+}
 
-//         /* save {q0-q7} */
-//         "sub sp, sp, #(8*16)\n"
-//         "stp q6, q7, [sp, #(6*16)]\n"
-//         "stp q4, q5, [sp, #(4*16)]\n"
-//         "stp q2, q3, [sp, #(2*16)]\n"
-//         "stp q0, q1, [sp, #(0*16)]\n"
-
-//         /* save {x1-x30} */
-//         "sub sp, sp, #(30*8)\n"
-//         // "stp fp, lr, [sp, #(28*8)]\n"
-//         "stp x29, x30, [sp, #(28*8)]\n"
-//         "stp x27, x28, [sp, #(26*8)]\n"
-//         "stp x25, x26, [sp, #(24*8)]\n"
-//         "stp x23, x24, [sp, #(22*8)]\n"
-//         "stp x21, x22, [sp, #(20*8)]\n"
-//         "stp x19, x20, [sp, #(18*8)]\n"
-//         "stp x17, x18, [sp, #(16*8)]\n"
-//         "stp x15, x16, [sp, #(14*8)]\n"
-//         "stp x13, x14, [sp, #(12*8)]\n"
-//         "stp x11, x12, [sp, #(10*8)]\n"
-//         "stp x9, x10, [sp, #(8*8)]\n"
-//         "stp x7, x8, [sp, #(6*8)]\n"
-//         "stp x5, x6, [sp, #(4*8)]\n"
-//         "stp x3, x4, [sp, #(2*8)]\n"
-//         "stp x1, x2, [sp, #(0*8)]\n"
-
-//         // C6.1.3
-//         // Use of the stack pointer
-//         // save x0 (and reserve sp, but this is trick.)
-//         "sub sp, sp, #(2*8)\n"
-//         "str x0, [sp, #8]\n");
-// }
-
-// __attribute__((__naked__)) static void ctx_restore() {
-//     __asm__ volatile(
-//         // C6.1.3
-//         // Use of the stack pointer
-//         // restore x0
-//         "ldr x0, [sp, #8]\n"
-//         "add sp, sp, #(2*8)\n"
-
-//         /* restore {x1-x30} */
-//         "ldp x1, x2, [sp], #16\n"
-//         "ldp x3, x4, [sp], #16\n"
-//         "ldp x5, x6, [sp], #16\n"
-//         "ldp x7, x8, [sp], #16\n"
-//         "ldp x9, x10, [sp], #16\n"
-//         "ldp x11, x12, [sp], #16\n"
-//         "ldp x13, x14, [sp], #16\n"
-//         "ldp x15, x16, [sp], #16\n"
-//         "ldp x17, x18, [sp], #16\n"
-//         "ldp x19, x20, [sp], #16\n"
-//         "ldp x21, x22, [sp], #16\n"
-//         "ldp x23, x24, [sp], #16\n"
-//         "ldp x25, x26, [sp], #16\n"
-//         "ldp x27, x28, [sp], #16\n"
-//         // "ldp fp, lr, [sp], #16\n"
-//         "ldp x29, x30, [sp], #16\n"
-
-//         /* restore {q0-q7} */
-//         "ldp q0, q1, [sp], #32\n"
-//         "ldp q2, q3, [sp], #32\n"
-//         "ldp q4, q5, [sp], #32\n"
-//         "ldp q6, q7, [sp], #32\n");
-// }
-
-// just like pre_call, wow!
 void function_context_begin_invocation(ZzHookFunctionEntry *entry, zpointer next_hop, RegState *rs,
                                        zpointer caller_ret_addr) {
     Xinfo("target %p call begin-invocation", entry->target_ptr);
