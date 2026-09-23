2ea6f548949381352fd4a961a14c66013313b4e3
mostwanted <ash.luna.shei@gmail.com>
2026-09-24 00:46:46 +0330

    Inline fb native sources for ARM64 build

--- VirtualApp/lib/src/main/jni/Android.mk
+++ VirtualApp/lib/src/main/jni/Android.mk
@@ -22,16 +22,17 @@ LOCAL_SRC_FILES := Jni/VAJni.cpp \
                    Substrate/SubstratePosixMemory.cpp \
 
 LOCAL_LDLIBS := -llog -latomic
-LOCAL_STATIC_LIBRARIES := fb
+
 
 ifneq ($(TARGET_ARCH),arm64)
 LOCAL_SRC_FILES += Substrate/hde64.c Substrate/SubstrateHook.cpp
 endif
 
 ifeq ($(TARGET_ARCH),arm64)
-LOCAL_C_INCLUDES += $(MAIN_LOCAL_PATH)/HookZz/include $(MAIN_LOCAL_PATH)/HookZz/src
+LOCAL_C_INCLUDES += $(MAIN_LOCAL_PATH)/HookZz/include $(MAIN_LOCAL_PATH)/HookZz/src $(MAIN_LOCAL_PATH)/fb/include
+LOCAL_CPPFLAGS += -frtti -fexceptions -std=gnu++11
 LOCAL_SRC_FILES += $(wildcard $(MAIN_LOCAL_PATH)/HookZz/src/*.c) $(wildcard $(MAIN_LOCAL_PATH)/HookZz/src/zzdeps/common/*.c) $(wildcard $(MAIN_LOCAL_PATH)/HookZz/src/zzdeps/linux/*.c) $(wildcard $(MAIN_LOCAL_PATH)/HookZz/src/zzdeps/posix/*.c) $(wildcard $(MAIN_LOCAL_PATH)/HookZz/src/platforms/arch-arm64/*.c) $(wildcard $(MAIN_LOCAL_PATH)/HookZz/src/platforms/backend-arm64/*.c) $(wildcard $(MAIN_LOCAL_PATH)/HookZz/src/platforms/backend-linux/*.c) $(wildcard $(MAIN_LOCAL_PATH)/HookZz/src/platforms/backend-posix/*.c)
+LOCAL_SRC_FILES += $(wildcard $(MAIN_LOCAL_PATH)/fb/*.cpp) $(wildcard $(MAIN_LOCAL_PATH)/fb/jni/*.cpp) $(wildcard $(MAIN_LOCAL_PATH)/fb/lyra/*.cpp)
 endif
 
-include $(MAIN_LOCAL_PATH)/fb/Android.mk
 include $(BUILD_SHARED_LIBRARY)
